3bd360240f93af62faaeab3b9963cd2bfe4a3d45
Tyler Roman <31106886+Syndicist@users.noreply.github.com>
2021-03-16 20:47:48 -0400

    Setting up arrays (works!)

--- ik_2/ik_2d.gd
+++ ik_2/ik_2d.gd
@@ -3,12 +3,12 @@ extends Node2D
 class_name IK2D
 
 export(bool) var IK_on := true
-export(NodePath) var root_bone_path
-export(Array) var joint_paths
-export(NodePath) var target_path
-var root_bone
+export(Array) var root_bone_paths : Array
+export(Array) var joint_paths : Array
+export(Array) var target_paths : Array
+var root_bones : Array
 var joints : Array
-var target
+var targets : Array
 
 
 
@@ -18,47 +18,60 @@ func _ready():
 
 func initialize():
 		process_priority = 1
+		root_bones.clear()
+		root_bone_paths.clear()
 		joints.clear()
 		joint_paths.clear()
-		target = get_node(target_path)
-		find_root(get_parent())
+		find_roots(get_parent())
 		validate_needed_nodes()
-		joint_paths.append(root_bone_path)
-		joints.append(root_bone)
-		initialize_bone(root_bone)
-		get_joints(root_bone)
+		var idx := 0
+		for root_bone in root_bones:
+			joint_paths.append([])
+			joints.append([])
+			initialize_bone(root_bone, idx)
+			get_joints(root_bone, idx)
+			idx += 1
 
-func get_joints(node):
+func get_joints(node, idx):
+	joint_paths[idx].append(get_path_to(node))
+	joints[idx].append(node)
+	joints.append(node)
 	for child in node.get_children():
-		if child is IKBone2D:
-			joint_paths.append(node.get_path_to(child))
-			joints.append(child)
-			get_joints(child)
+		if child is IKBone2D or child is IKEnd2D:
+			get_joints(child, idx)
+
 
 func validate_needed_nodes():
-	if not root_bone:
-		print(get_parent().get_node(root_bone_path).name)
-		root_bone = get_parent().get_node(root_bone_path)
-	if not target:
-		target = get_node(target_path)
+	var idx := 0
+	for child in get_parent().get_children():
+		if child is IKRoot2D:
+			idx += 1
+	if root_bones.size() != idx:
+		root_bones.clear()
+		for root_bone_path in root_bone_paths:
+			root_bones.append(get_node(root_bone_path))
+	if targets.size() != idx:
+		targets.clear()
+		for target_path in target_paths:
+			targets.append(get_node(target_path))
 
 func _get_configuration_warning():
-	if not root_bone_path:
-		return "Can't find Root"
-	if not target_path:
-		return "No Target assigned"
+	if not root_bone_paths:
+		return "Can't find Roots"
+	if target_paths.size() != root_bone_paths.size():
+		return "Not enough assigned Targets"
 	return ''
 
-func find_root(node):
+func find_roots(node):
+	var idx := 0
 	for child in node.get_children():
 		if child is IKRoot2D:
-			if not child.ik_handler:
-				root_bone_path = get_parent().get_path_to(child);
-				root_bone = child
-				child.ik_handler = self
-				child.IK_handler_path = child.get_path_to(self)
-				return true;
-	return false;
+			root_bone_paths.append(get_path_to(child));
+			root_bones.append(child)
+			child.ik_handler = self
+			child.IK_handler_path = child.get_path_to(self)
+			child.root_idx = idx
+			idx += 1
 
 func find_end(node):
 	node.end_bone = find_end_recurse(node)
@@ -67,36 +80,41 @@ func find_end(node):
 	return false
 
 func find_end_recurse(node):
-	if node is IKEnd2D:
-		return node
 	for child in node.get_children():
+		print(child.name)
 		if child is IKBone2D:
 			find_end_recurse(child)
+		if child is IKEnd2D:
+			return node
 	return null
 
-func initialize_bone(bone):
+func initialize_bone(bone, root_idx):
 	for child in bone.get_children():
-		if child is IKBone2D:
+		if child is IKBone2D or child is IKEnd2D:
 			bone.child_bone_path = bone.get_path_to(child)
 			bone.child_bone = child
 			bone.length = bone.global_position.distance_to(child.global_position)
-			initialize_bone(child)
+			bone.IK_handler_path = bone.get_path_to(self)
+			bone.ik_handler = self
+			initialize_bone(child, root_idx)
 		if child is IKEnd2D:
-			root_bone.end_bone = child
-			root_bone.end_bone_path = root_bone.get_path_to(child)
-			root_bone.ready = true
+			root_bones[root_idx].end_bone = child
+			root_bones[root_idx].end_bone_path = root_bones[root_idx].get_path_to(child)
+			root_bones[root_idx].ready = true
 
 
 func _process(_delta):
 	if Engine.editor_hint or not Engine.editor_hint:
-		if target and root_bone and IK_on:
-			calculate_ik()
-
+		if targets.size() == root_bones.size() and root_bones and IK_on:
+			pass
+			#calculate_ik()
+"""
 func calculate_ik():
 	var target_dist = root_bone.global_position.distance_to(target.global_position)
 	var reach = 0.0
 	for bone in joints:
-		reach += bone.length
+		if not bone is IKEnd2D:
+			reach += bone.length
 	var rel_dist = root_bone.end_bone.global_position.distance_to(target.global_position)
 	if rel_dist != 0:
 		if target_dist > reach:
@@ -126,3 +144,4 @@ func calculate_ik():
 				var lambda = joints[i].length / rel_dist
 				joints[i+1].global_position = (1 - lambda) * joints[i].global_position + lambda * joints[i+1].global_position
 				
+"""
--- ik_2/ik_bone_2d.gd
+++ ik_2/ik_bone_2d.gd
@@ -2,15 +2,23 @@ tool
 extends Bone2D
 class_name IKBone2D
 
+export(NodePath) var IK_handler_path
 export(float) var length := 16.0
 export(NodePath) var child_bone_path
 var child_bone
 var child_prev_position
+var ik_handler
 
 func _ready():
 	if Engine.editor_hint or not Engine.editor_hint:
+		validate_needed_nodes()
 		if child_bone:
 			child_prev_position = child_bone.position
+	if Engine.editor_hint:
+		if ik_handler:
+			if ik_handler.root_bone != self:
+				ik_handler = null
+				IK_handler_path = null
 
 func _process(_delta):
 	if Engine.editor_hint or not Engine.editor_hint:
@@ -18,3 +26,9 @@ func _process(_delta):
 			length = global_position.distance_to(child_bone.global_position)
 			default_length = global_position.distance_to(child_bone.global_position)
 			child_prev_position = child_bone.position
+		if child_bone is IKEnd2D:
+			look_at(ik_handler.target.global_position)
+
+func validate_needed_nodes():
+	if not ik_handler:
+		ik_handler = get_node(IK_handler_path)
