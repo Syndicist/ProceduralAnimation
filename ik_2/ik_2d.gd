aa9ecb994a770c1ffd05e1926a3b7cf0802a218e
Tyler Roman <31106886+Syndicist@users.noreply.github.com>
2021-03-16 21:59:49 -0400

    Implemented multi-arm handling
    sub-bases possibly half-implemented

--- ik_2/ik_2d.gd
+++ ik_2/ik_2d.gd
@@ -3,12 +3,12 @@ extends Node2D
 class_name IK2D
 
 export(bool) var IK_on := true
-export(NodePath) var root_bone_path
-export(Array) var joint_paths
-export(NodePath) var target_path
-var root_bone
+export(Array) var root_bone_paths : Array
+export(Array) var joint_paths : Array
+export(Array) var target_paths : Array
+var root_bones : Array
 var joints : Array
-var target
+var targets : Array
 
 
 
@@ -18,47 +18,59 @@ func _ready():
 
 func initialize():
 		process_priority = 1
-		joints.clear()
-		joint_paths.clear()
-		target = get_node(target_path)
-		find_root(get_parent())
+		root_bones = []
+		root_bone_paths = []
+		joints = []
+		joint_paths = []
+		find_roots(get_parent())
 		validate_needed_nodes()
-		joint_paths.append(root_bone_path)
-		joints.append(root_bone)
-		initialize_bone(root_bone)
-		get_joints(root_bone)
+		var idx := 0
+		for root_bone in root_bones:
+			joint_paths.append([])
+			joints.append([])
+			initialize_bone(root_bone, idx)
+			get_joints(root_bone, idx)
+			idx += 1
 
-func get_joints(node):
+func get_joints(node, idx):
+	joint_paths[idx].append(get_path_to(node))
+	joints[idx].append(node)
 	for child in node.get_children():
 		if child is IKBone2D or child is IKEnd2D:
-			joint_paths.append(node.get_path_to(child))
-			joints.append(child)
-			get_joints(child)
+			get_joints(child, idx)
+
 
 func validate_needed_nodes():
-	if not root_bone:
-		print(get_parent().get_node(root_bone_path).name)
-		root_bone = get_parent().get_node(root_bone_path)
-	if not target:
-		target = get_node(target_path)
+	var idx := 0
+	for child in get_parent().get_children():
+		if child is IKRoot2D:
+			idx += 1
+	if root_bones.size() != idx:
+		root_bones.clear()
+		for root_bone_path in root_bone_paths:
+			root_bones.append(get_node(root_bone_path))
+	if targets.size() != idx:
+		targets.clear()
+		for target_path in target_paths:
+			targets.append(get_node(target_path))
 
 func _get_configuration_warning():
-	if not root_bone_path:
-		return "Can't find Root"
-	if not target_path:
-		return "No Target assigned"
+	if not root_bone_paths:
+		return "Can't find Roots"
+	if target_paths.size() != root_bone_paths.size():
+		return "Not enough assigned Targets"
 	return ''
 
-func find_root(node):
+func find_roots(node):
+	var idx := 0
 	for child in node.get_children():
 		if child is IKRoot2D:
-			if not child.ik_handler:
-				root_bone_path = get_parent().get_path_to(child);
-				root_bone = child
-				child.ik_handler = self
-				child.IK_handler_path = child.get_path_to(self)
-				return true;
-	return false;
+			root_bone_paths.append(get_path_to(child));
+			root_bones.append(child)
+			child.ik_handler = self
+			child.IK_handler_path = child.get_path_to(self)
+			child.root_idx = idx
+			idx += 1
 
 func find_end(node):
 	node.end_bone = find_end_recurse(node)
@@ -75,7 +87,7 @@ func find_end_recurse(node):
 			return node
 	return null
 
-func initialize_bone(bone):
+func initialize_bone(bone, root_idx):
 	for child in bone.get_children():
 		if child is IKBone2D or child is IKEnd2D:
 			bone.child_bone_path = bone.get_path_to(child)
@@ -83,36 +95,46 @@ func initialize_bone(bone):
 			bone.length = bone.global_position.distance_to(child.global_position)
 			bone.IK_handler_path = bone.get_path_to(self)
 			bone.ik_handler = self
-			initialize_bone(child)
+			bone.root_idx = root_idx
+			initialize_bone(child, root_idx)
 		if child is IKEnd2D:
-			root_bone.end_bone = child
-			root_bone.end_bone_path = root_bone.get_path_to(child)
-			root_bone.ready = true
+			root_bones[root_idx].end_bone = child
+			root_bones[root_idx].end_bone_path = root_bones[root_idx].get_path_to(child)
+			root_bones[root_idx].ready = true
 
 
 func _process(_delta):
 	if Engine.editor_hint or not Engine.editor_hint:
-		if target and root_bone and IK_on:
+		if targets.size() == root_bones.size() and root_bones and IK_on:
 			calculate_ik()
 
+
 func calculate_ik():
-	var rel_dist = root_bone.end_bone.global_position.distance_to(target.global_position)
-	#target is reachable
-	var initial_pos = root_bone.global_position
-	
+	var idx := 0
+	var centroid := Vector2(0,0)
+	var initial_positions := []
 	#forward reaching
 	#set end position to target
-	root_bone.end_bone.global_position = target.global_position
-	for i in range(joints.size()-2, 0, -1):
-		rel_dist = joints[i].global_position.distance_to(target.global_position)
-		var lambda = joints[i].length / rel_dist
-		joints[i].global_position = (1 - lambda) * joints[i+1].global_position + lambda * joints[i].global_position
-
+	for root_bone in root_bones:
+		#initial_positions.append(root_bone.global_position)
+		root_bone.end_bone.global_position = targets[idx].global_position
+		for i in range(joints[idx].size()-2, 0, -1):
+			var rel_dist = joints[idx][i].global_position.distance_to(targets[idx].global_position)
+			var lambda = joints[idx][i].length / rel_dist
+			joints[idx][i].global_position = (1 - lambda) * joints[idx][i+1].global_position + lambda * joints[idx][i].global_position
+		centroid += root_bone.global_position
+		print(to_local(centroid))
+		idx += 1
+	
+	centroid /= root_bones.size()
 	#backward reaching
 	#set root back to origin
-	root_bone.global_position = initial_pos
-	for i in joints.size()-1:
-		rel_dist = joints[i+1].global_position.distance_to(joints[i].global_position)
-		var lambda = joints[i].length / rel_dist
-		joints[i+1].global_position = (1 - lambda) * joints[i].global_position + lambda * joints[i+1].global_position
-		
+	idx = 0
+	for root_bone in root_bones:
+		#root_bone.global_position = initial_positions[idx]
+		root_bone.global_position = centroid
+		for i in joints[idx].size()-1:
+			var rel_dist = joints[idx][i+1].global_position.distance_to(joints[idx][i].global_position)
+			var lambda = joints[idx][i].length / rel_dist
+			joints[idx][i+1].global_position = (1 - lambda) * joints[idx][i].global_position + lambda * joints[idx][i+1].global_position
+		idx += 1
